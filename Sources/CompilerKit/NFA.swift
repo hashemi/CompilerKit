46df2ca5e65adb768d178fac9e92bc3cff98dd03
Ahmad Alhashemi <ahmad@ahmadh.com>
2018-03-22 09:30:21 -0400

    NFA should exhaustively find reachable states

--- Sources/CompilerKit/NFA.swift
+++ Sources/CompilerKit/NFA.swift
@@ -14,10 +14,14 @@ struct NFA {
         var states: Set<Int> = [initial]
         for scalar in s.unicodeScalars {
             // add all states reachable by epsilon transitions
-            states.formUnion(
-                edges
-                    .filter { states.contains($0.from) && $0.scalar == nil }
-                    .map { $0.to })
+            var exploreNext: [Int] = Array(states)
+            while let explore = exploreNext.popLast() {
+                let newReachableStates = edges
+                        .filter { $0.from == explore && !states.contains($0.to) && $0.scalar == nil }
+                        .map { $0.to }
+                exploreNext.append(contentsOf: newReachableStates)
+                states.formUnion(newReachableStates)
+            }
             
             // new set of states as allowed by current scalar in string
             states = Set(edges
